cf7715f44290dfd69781c0932f7fafc2481aeaa6
Jailson Jr <jailsonorr@gmail.com>
2021-07-28 23:53:15 -0300

    Add cors to api

--- api/pages/api/[year].js
+++ api/pages/api/[year].js
@@ -1,6 +1,10 @@
 import json_data from "../../data/campeonato-brasileiro.json";
+import Cors from 'cors';
 
-export default function handler(req, res) {
+export  default function handler(req, res) {
+  Cors({
+    methods: ['GET', 'POST', 'OPTIONS']
+  });
   let data = json_data[req.query.year] || {};
   res.status(200).json(data);  
 }
