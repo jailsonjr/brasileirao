4e7e1226189375edf6105c160a5dc6cd20469d5b
Jailson Jr <jailsonorr@gmail.com>
2021-08-03 19:36:14 -0300

    Fix Responsive design in mobile phones

--- frontend/Components/Navigation.js
+++ frontend/Components/Navigation.js
@@ -6,7 +6,7 @@ export default function Navigation(props) {
   const years = [2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015];
 
   return (
-    <div className={styles.appNav}>
+    <nav className={styles.appNav}>
       {
         years.map( year => {
           let activeLink = props.activeYear == year;
@@ -16,6 +16,6 @@ export default function Navigation(props) {
         })
       }
       
-    </div>
+    </nav>
   )
 }
